44c038ed60df40f5fd9d3bd9f3f255d201635789
Sam Ripka <ripka.semyon@gmail.com>
2023-12-05 20:32:16 -0400

    first commit

---
+++ src/main/java/com/comit/webblog/ComitSpringSrApplication.java
@@ -0,0 +1,13 @@
+package com.comit.webblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitSpringSrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitSpringSrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comit/webblog/ComitSpringSrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comit.webblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComitSpringSrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
