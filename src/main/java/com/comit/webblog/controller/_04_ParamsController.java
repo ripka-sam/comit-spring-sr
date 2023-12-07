1dad5731268c30c4235b18962d2202a08153aa1b
Sam Ripka <ripka.semyon@gmail.com>
2023-12-06 23:38:29 -0400

    Add Model

---
+++ src/main/java/com/comit/webblog/controller/_04_ParamsController.java
@@ -0,0 +1,23 @@
+package com.comit.webblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class _04_ParamsController {
+
+	@GetMapping("/parameters")
+	ModelAndView hello(@RequestParam String first, @RequestParam String second) {
+		
+		System.out.println("First Parameter: "+first);
+		System.out.println("SecondParameter: "+second);
+		
+		ModelAndView mv = new ModelAndView("parameters"); //HTML template name.
+		mv.addObject("first",first);
+		mv.addObject("second",second);
+		
+		return mv;
+	}
+}
